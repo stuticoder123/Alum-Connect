c2d0a97780eec7627a64f36606e8964a0f486efa
Stuti Gupta <juhiexcusestuti@gmail.com>
2026-07-06 18:11:13 +0530

    Add AnnouncementBanner to Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import AnnouncementBanner from "./AnnouncementBanner";
 
 const Layout: React.FC = () => {
   const location = useLocation();
@@ -10,12 +11,14 @@ const Layout: React.FC = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
+      <AnnouncementBanner />
       <Navbar transparent={isHomePage} />
       <main className="flex-grow">
         <Outlet />
       </main>
+
       <Footer />
-    </div>
+</div>
   );
 };
 
